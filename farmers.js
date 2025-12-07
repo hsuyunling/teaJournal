61f7d770f6f5dc88be953dca565c3d72a4429c60
yunlinghsu <113306005@g.nccu.edu.tw>
2025-12-07 16:44:26 +0800

    add introduction in aboutUs.html

--- farmers.js
+++ farmers.js
@@ -3,7 +3,7 @@ const farmers = [
         id: 1,
         farmer: "永安食蛇龜茶園",
         img: "./images/farmer1.jpg",
-        location: "#",
+        location: "https://www.google.com/maps/place/金龍茶園/@24.9746436,121.5777032,17z/data=!3m1!4b1!4m6!3m5!1s0x346801d37c418909:0x11f2197d2f7da8bb!8m2!3d24.9746436!4d121.5802781!16s%2Fg%2F11cscc6t5r?entry=ttu&g_ep=EgoyMDI1MTIwMi4wIKXMDSoASAFQAw%3D%3D",
         buyLink: "farmerProduct.html?farmer=永安食蛇龜茶園",
         journalLink: "detailedInformation.html?farmer=永安食蛇龜茶園"
     },
